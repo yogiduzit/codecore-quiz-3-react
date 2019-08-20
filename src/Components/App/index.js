f4f92042463db684cccb9de3cfb84c30335c731c
yogiduzit <yverma2@my.bcit.ca>
2019-08-19 18:43:56 -0700

    Added localStorage method for currentUser

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,53 +1,56 @@
 import React from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route, Redirect} from 'react-router-dom';
 import PostsIndexPage from '../PostsIndexPage';
 import PostsShowPage from '../PostsShowPage';
 import SignInPage from '../SignInPage';
 import Navbar from '../Navbar';
-import {Session} from '../../api/requests';
+import AuthRoute from '../AuthRoute';
+import NewPostPage from '../NewPostPage';
+
+import { User, Session } from '../../api/requests';
+
+
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      currentUser: null
+      currentUser: localStorage.currentUser
     }
 
-    this.signIn = this.signIn.bind(this);
+    this.getCurrentUser = this.getCurrentUser.bind(this);
   }
 
   componentDidMount() {
-    this.setState({
-      currentUser: localStorage.currentUser
+
+  }
+
+  getCurrentUser(arg) {
+    User.current().then(data => {
+        this.setState({
+          currentUser: data.current_user ? data.current_user.id : null
+        });
+
+        return arg;
     })
   }
 
-  signIn(params) {
-    Session.create(params)
-    .then(data => {
-      if (data.id) {
-        localStorage.setItem("currentUser", data.id)
-        this.setState({
-          currentUser: data.id
-        })
-      }
-    });
-  }
 
 
   render() {
     return(
       <div className="main">
-
         <BrowserRouter>
-          <Navbar isAuth={this.state.currentUser ? true : false}/>
+          <Navbar isAuth={this.state.currentUser ? true : false} getCurrentUser={this.getCurrentUser} />
           <Route exact path="/posts" component={PostsIndexPage}></Route>
           <Route exact path="/posts/:id" component={PostsShowPage}></Route>
-          <Route exact path="/sign_in" component={() => <SignInPage signIn={this.signIn} />}></Route>
+          <Route exact path="/sign_in" render={(routeProps) => <SignInPage onSignIn={this.getCurrentUser} {...routeProps}/>}></Route>
+          <AuthRoute path="/post/new" component={NewPostPage} isAuth={this.state.currentUser} />
         </BrowserRouter>
       </div>
     )
   }
 }
+
 
