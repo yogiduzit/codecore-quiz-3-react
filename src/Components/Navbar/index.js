f5a84eeb9088439224fe9c830b595c110b3a5399
yogiduzit <yverma2@my.bcit.ca>
2019-08-19 18:41:24 -0700

    Added sign out method

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Session } from '../../api/requests';
 
 export default function Navbar(props) {
+  function signOut() {
+    Session.destroy().then(data => {
+      props.getCurrentUser();
+      localStorage.removeItem("currentUser");
+    });
+  }
   return(
     <div className="nav-container">
       <nav className="navbar">
@@ -11,7 +18,7 @@ export default function Navbar(props) {
         |
       </nav>
         { !props.isAuth ? ( <nav><Link to="/sign_up">Sign Up</Link> <Link to='/sign_in'>Sign In</Link></nav>)
-        : (<nav><Link to="/sign_out" onClick={props.signOut}>Sign Out</Link> | <Link to='/sign_in'>My Profile</Link></nav>)
+        : (<nav><Link to="/" onClick={signOut}>Sign Out</Link> | <Link to='/sign_in'>My Profile</Link></nav>)
         }
     </div>
   )
